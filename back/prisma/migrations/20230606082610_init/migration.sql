c5ae03b3d35a83246a0f2ad78a3cd93efbd1f73d
Michael De Madet <michaeldemadet@gmail.com>
2023-06-06 12:06:05 +0200

    add tables

---
+++ back/prisma/migrations/20230606082610_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "birthdate" TIMESTAMP(3) NOT NULL,
+    "hiredate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
