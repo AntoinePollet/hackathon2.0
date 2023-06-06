3183e51a1a80d000f19c54208377735f418f351b
Antoine lin <lin.antoine.pro@gmail.com>
2023-06-06 14:31:09 +0200

    base

--- front/src/components/Structure/Sidebar.vue
+++ front/src/components/Structure/Sidebar.vue
@@ -9,20 +9,26 @@
         </v-list-item>
 
         <v-list density="compact" :nav="true">
-          <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
-            title="John Leider"
-            :nav="true"
-            to="profile"
-          />
+          <router-link :to="{ name: 'profile-show' }">
+            <v-list-item
+              prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+              :nav="true"
+              value="profile"
+            >
+                John Leider
+            </v-list-item>
+          </router-link>
           <v-divider class="border-black"></v-divider>
-          <v-list-item
+          <router-link
             v-for="item in items"
-            :prepend-icon="item.icon"
-            :title="item.title"
-            :value="item.value"
-            :to="item.to"
-          />
+            :to="{ name: item.to }"
+          >
+            <v-list-item
+              :prepend-icon="item.icon"
+              :title="item.title"
+              :value="item.value"
+            />
+          </router-link>
           <v-divider class="border-black"></v-divider>
           <div>
             <v-list-item class="flex">
@@ -31,13 +37,13 @@
               </div>
             </v-list-item>
             <v-divider class="border-black"></v-divider>
-            <v-list-item
-              v-for="item in clientItems"
-              :prepend-icon="item.icon"
-              :title="item.title"
-              :value="item.value"
-              :to="item.to"
-            />
+            <router-link v-for="item in clientItems" :to="{ name: item.to }">
+              <v-list-item
+                :prepend-icon="item.icon"
+                :title="item.title"
+                :value="item.value"
+              />
+            </router-link>
           </div>
           <v-divider class="border-black"></v-divider>
           <div>
@@ -47,18 +53,23 @@
               </div>
             </v-list-item>
             <v-divider class="border-black"></v-divider>
-            <v-list-item
+            <router-link
               v-for="item in adminItems"
-              :prepend-icon="item.icon"
-              :title="item.title"
-              :value="item.value"
-              :to="item.to"
-            />
+              :to="{ name: item.to }"
+            >
+              <v-list-item
+                :prepend-icon="item.icon"
+                :title="item.title"
+                :value="item.value"
+              />
+            </router-link>
           </div>
           <v-divider class="border-black"></v-divider>
           <v-list-item
             prepend-icon="a"
             title="Log out"
+            value="logout"
+            @click="null"
           />
         </v-list>
       </div>
@@ -70,23 +81,22 @@
 import {onMounted, ref} from "vue";
 
 const items = ref([
-  { title: 'My Account', icon: 'mdi-account', to: '', value: 'my-account' },
-  { title: 'Planning', icon: 'mdi-account', to: '', value: 'my-account' },
-  { title: 'Trainings', icon: 'mdi-account', to: '', value: 'my-account' },
-  { title: 'Achievements', icon: 'mdi-account', to: '', value: 'my-account' },
-  { title: 'Evaluations', icon: 'mdi-account', to: '', value: 'my-account' },
+  { title: 'Planning', icon: 'mdi-account', to: 'plannings-list', value: 'my-planning' },
+  { title: 'Trainings', icon: 'mdi-account', to: 'trainings-list', value: 'my-training' },
+  { title: 'Achievements', icon: 'mdi-account', to: 'achievements-list', value: 'my-achievement' },
+  { title: 'Evaluations', icon: 'mdi-account', to: 'evaluations-list', value: 'my-evaluation' },
 ])
 
 const adminItems = ref([
-  { title: 'Users', icon: 'mdi-account-group-outline', to: '', value: 'users' },
-  { title: 'Plannings', icon: 'mdi-account-group-outline', to: '', value: 'users' },
-  { title: 'Achievements', icon: 'mdi-account-group-outline', to: '', value: 'users' },
-  { title: 'Trainings', icon: 'mdi-account-group-outline', to: '', value: 'users' },
+  { title: 'Users', icon: 'mdi-account-group-outline', to: 'admin-users-list', value: 'users' },
+  { title: 'Plannings', icon: 'mdi-account-group-outline', to: 'admin-plannings-list', value: 'plannings' },
+  { title: 'Achievements', icon: 'mdi-account-group-outline', to: 'admin-achievements-list', value: 'achievements' },
+  { title: 'Trainings', icon: 'mdi-account-group-outline', to: 'admin-trainings-list', value: 'trainings' },
 ]);
 
 const clientItems = ref([
-  { title: 'My Account', icon: 'mdi-account', to: '', value: 'my-account' },
-  { title: 'Planning', icon: 'mdi-account', to: '', value: 'my-account' },
+  { title: 'My Account', icon: 'mdi-account', to: 'client-profile-show', value: 'client-account' },
+  { title: 'Planning', icon: 'mdi-account', to: 'client-plannings-list', value: 'client-plannings' },
 ]);
 
 onMounted(() => {
--- front/src/router/routes.ts
+++ front/src/router/routes.ts
@@ -9,6 +9,30 @@ import UserProfileEdit from "@/components/Profile/Edit.vue";
 import UsersList from "@/components/Admin/User/List.vue";
 import UsersShow from "@/components/Admin/User/Show.vue";
 import UsersEdit from "@/components/Admin/User/Edit.vue";
+import AdminPlanningsList from "@/components/Admin/Planning/List.vue";
+import AdminPlanningsShow from "@/components/Admin/Planning/Show.vue";
+import AdminPlanningsEdit from "@/components/Admin/Planning/Edit.vue";
+import AdminAchievementsList from "@/components/Admin/Achievement/List.vue";
+import AdminAchievementsShow from "@/components/Admin/Achievement/Show.vue";
+import AdminAchievementsEdit from "@/components/Admin/Achievement/Edit.vue";
+import AdminTrainingsList from "@/components/Admin/Training/List.vue";
+import AdminTrainingsShow from "@/components/Admin/Training/Show.vue";
+import AdminTrainingsEdit from "@/components/Admin/Training/Edit.vue";
+import PlanningsList from "@/components/User/Planning/List.vue";
+import PlanningsShow from "@/components/User/Planning/Show.vue";
+import PlanningsEdit from "@/components/User/Planning/Edit.vue";
+import AchievementsList from "@/components/User/Achievement/List.vue";
+import AchievementsShow from "@/components/User/Achievement/Show.vue";
+import AchievementsEdit from "@/components/User/Achievement/Edit.vue";
+import TrainingsList from "@/components/User/Training/List.vue";
+import TrainingsShow from "@/components/User/Training/Show.vue";
+import TrainingsEdit from "@/components/User/Training/Edit.vue";
+import EvaluationsList from "@/components/User/Evaluation/List.vue";
+import EvaluationsShow from "@/components/User/Evaluation/Show.vue";
+import EvaluationsEdit from "@/components/User/Evaluation/Edit.vue";
+import ClientPlanningsList from "@/components/Client/Planning/List.vue";
+import ClientPlanningsShow from "@/components/Client/Planning/Show.vue";
+import ClientPlanningsEdit from "@/components/Client/Planning/Edit.vue";
 
 export default [
     {
@@ -18,6 +42,18 @@ export default [
         children: [
             { path: 'profile', name: 'profile-show', component: UserProfileShow},
             { path: 'profile/edit', name: 'profile-edit', component: UserProfileEdit},
+            { path: 'plannings', name: 'plannings-list', component: PlanningsList },
+            { path: 'planning/:id', name: 'plannings-show', component: PlanningsShow },
+            { path: 'planning/:id/edit', name: 'plannings-edit', component: PlanningsEdit },
+            { path: 'achievements', name: 'achievements-list', component: AchievementsList },
+            { path: 'achievement/:id', name: 'achievements-show', component: AchievementsShow },
+            { path: 'achievement/:id/edit', name: 'achievements-edit', component: AchievementsEdit },
+            { path: 'trainings', name: 'trainings-list', component: TrainingsList },
+            { path: 'training/:id', name: 'trainings-show', component: TrainingsShow },
+            { path: 'training/:id/edit', name: 'trainings-edit', component: TrainingsEdit },
+            { path: 'evaluations', name: 'evaluations-list', component: EvaluationsList },
+            { path: 'evaluation/:id', name: 'evaluations-show', component: EvaluationsShow },
+            { path: 'evaluation/:id/edit', name: 'evaluations-edit', component: EvaluationsEdit },
         ]
     },
     {
@@ -25,8 +61,11 @@ export default [
         name: '',
         component: Client,
         children: [
-            { path: 'profile', name: 'profile-show', component: UserProfileShow},
-            { path: 'profile/edit', name: 'profile-edit', component: UserProfileEdit},
+            { path: 'profile', name: 'client-profile-show', component: UserProfileShow},
+            { path: 'profile/edit', name: 'client-profile-edit', component: UserProfileEdit},
+            { path: 'plannings', name: 'client-plannings-list', component: ClientPlanningsList },
+            { path: 'planning/:id', name: 'client-plannings-show', component: ClientPlanningsShow },
+            { path: 'planning/:id/edit', name: 'client-plannings-edit', component: ClientPlanningsEdit },
         ]
     },
     {
@@ -34,9 +73,18 @@ export default [
         name: '',
         component: Admin,
         children: [
-            { path: 'users', name: 'users-list', component: UsersList },
-            { path: 'user/:id', name: 'users-show', component: UsersShow },
-            { path: 'user/:id/edit', name: 'users-edit', component: UsersEdit },
+            { path: 'users', name: 'admin-users-list', component: UsersList },
+            { path: 'user/:id', name: 'admin-users-show', component: UsersShow },
+            { path: 'user/:id/edit', name: 'admin-users-edit', component: UsersEdit },
+            { path: 'plannings', name: 'admin-plannings-list', component: AdminPlanningsList },
+            { path: 'planning/:id', name: 'admin-plannings-show', component: AdminPlanningsShow },
+            { path: 'planning/:id/edit', name: 'admin-plannings-edit', component: AdminPlanningsEdit },
+            { path: 'achievements', name: 'admin-achievements-list', component: AdminAchievementsList },
+            { path: 'achievement/:id', name: 'admin-achievements-show', component: AdminAchievementsShow },
+            { path: 'achievement/:id/edit', name: 'admin-achievements-edit', component: AdminAchievementsEdit },
+            { path: 'trainings', name: 'admin-trainings-list', component: AdminTrainingsList },
+            { path: 'training/:id', name: 'admin-trainings-show', component: AdminTrainingsShow },
+            { path: 'training/:id/edit', name: 'admin-trainings-edit', component: AdminTrainingsEdit },
         ]
     },
     {
@@ -44,7 +92,7 @@ export default [
         name: '',
         component: Session,
         children: [
-            { path: 'signin', name: 'signin', component: Signin },
+            { path: 'signins', name: 'signin', component: Signin },
             { path: 'password-forgot', name: 'password-forgot', component: PasswordForgot },
         ]
     }
