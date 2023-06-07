b3e9cd843ac8e38e0f68d28dad22df7aee7a8bb6
Antoine lin <lin.antoine.pro@gmail.com>
2023-06-07 11:29:53 +0200

    news card

--- front/src/components/User/News/List.vue
+++ front/src/components/User/News/List.vue
@@ -4,12 +4,35 @@
       News
     </div>
     <div class="bg-red-100 flex-grow-1">
-      eaz
+      <v-card>
+        <div
+          class="p-5"
+        >
+          <div class="flex items-center mb-4 ">
+            <div class="px-3 py-1 rounded-full bg-h-blue text-white font-bold">
+              Cat 1
+            </div>
+          </div>
+          <div class="flex items-center mb-4">
+            <Icon icon="ri:add-circle-fill" class="h-8 w-8 mr-4"/>
+            Title
+          </div>
+          <div>
+            DescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptiondddddDescriptionddddd
+          </div>
+          <div class="flex justify-end w-full">
+            <div>
+              01/01/2001
+            </div>
+          </div>
+        </div>
+      </v-card>
     </div>
   </div>
 </template>
 
 <script setup>
+import {Icon} from "@iconify/vue";
 </script>
 
 <style scoped>
