b562e9844776d8f5f1a3dd6e8ee0cc358249cb03
Antoine <pollet.antoine.alexis@gmail.com>
2023-06-07 13:56:09 +0200

    Rebase by antoine

--- src/views/Client/Client.vue
+++ src/views/Client/Client.vue
@@ -1,23 +1,17 @@
 <template>
-  <div class="flex h-screen w-screen">
-    <div class="h-full">
-      <sidebar />
+    <div class="flex h-screen w-screen">
+        <div class="h-full">
+            <sidebar />
+        </div>
+        <div class="w-full h-screen overflow-auto">
+            <router-view class="w-full" />
+        </div>
     </div>
-    <div class="w-full h-screen overflow-auto">
-      <router-view class="w-full"/>
-    </div>
-    <div class="h-full">
-      <sidebar-notification />
-    </div>
-  </div>
 </template>
 
-<script setup>
-import Sidebar from "@/components/Structure/Sidebar.vue";
-import SidebarNotification from "@/components/Structure/SidebarNotification.vue";
+<script setup lang="ts">
+import Sidebar from "@/components/Structure/Sidebar.vue"
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
