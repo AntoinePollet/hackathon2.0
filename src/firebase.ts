2baed75d5063c0e2364b5e84e684594c7ca94e98
Antoine <pollet.antoine.alexis@gmail.com>
2023-06-07 12:18:32 +0200

    Fil actualité

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, orderBy, query, limit, getDocs } from "firebase/firestore";
+import { useCollection } from "vuefire"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,5 +25,6 @@ const firestoreDB = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
 
 export { firestoreDB, auth };
+export const eventsRef = useCollection(collection(firestoreDB, "events"))
 
 export default firebaseApp;
--- src/views/events.vue
+++ src/views/events.vue
@@ -2,14 +2,17 @@
 import { useEventStore } from '@/stores/event';
 import { onMounted } from 'vue';
 import { EventTypeEnum } from "@/interfaces/event"
+import { eventsRef } from "@/firebase"
+import event from '@/components/event.vue';
+import { storeToRefs } from 'pinia';
 
 const eventStore = useEventStore();
-const { publishEvent, getEvents, watchEventChanges } = eventStore;
+const { sortedEvents } = storeToRefs(eventStore);
 
 onMounted(async () => {
     try {
         // await getEvents();
-        publishEvent({ title: "New user", description: "Hello", type: EventTypeEnum.START_OF_MISSION });
+        // publishEvent({ title: "New user", description: "Hello", type: EventTypeEnum.START_OF_MISSION });
         // watchEventChanges();
     } catch (error) {
 
@@ -18,4 +21,13 @@ onMounted(async () => {
 
 </script>
 
-<template></template>
+<template>
+    <div class="text-center">
+        <h1 class="font-bold text-2xl py-3">Fil d'actualité</h1>
+    </div>
+    <div class="overflow-auto" style="height: calc(100vh - 56px)">
+        <div class="flex flex-col gap-y-3 p-5 w-3/4 m-auto">
+            <event v-for="event in sortedEvents" :key="event.id" :event="event"></event>
+        </div>
+    </div>
+</template>
