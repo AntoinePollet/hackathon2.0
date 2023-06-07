ae323f53e095b99068371a0acd741360e0803e3a
NilsMillot <compte.mxs@hotmail.fr>
2023-06-07 15:24:10 +0200

    add admin protected page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,27 +1,33 @@
-import Home from "@/components/home.vue"
-import Events from "@/views/events.vue"
-import Register from "@/views/register.vue"
-import Login from "@/views/login.vue"
+import Home from "@/components/home.vue";
+import Events from "@/views/events.vue";
+import Register from "@/views/register.vue";
+import Login from "@/views/login.vue";
+import Admin from "@/views/admin.vue";
 
 export default [
     {
-        path: '/',
-        name: 'home',
-        component: Home
+        path: "/",
+        name: "home",
+        component: Home,
     },
     {
-        path: '/events',
-        name: 'events',
-        component: Events
+        path: "/events",
+        name: "events",
+        component: Events,
     },
     {
-        path: '/register',
-        name: 'register',
-        component: Register
+        path: "/register",
+        name: "register",
+        component: Register,
     },
     {
-        path: '/login',
-        name: 'login',
-        component: Login
+        path: "/login",
+        name: "login",
+        component: Login,
     },
-]
+    {
+        path: "/admin",
+        name: "admin",
+        component: Admin,
+    },
+];
