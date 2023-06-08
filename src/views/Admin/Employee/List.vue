692a187e84c9ab39a55fb1216878f7dd058b1b71
NilsMillot <compte.mxs@hotmail.fr>
2023-06-08 02:12:59 +0200

    update list users for admin vue

--- src/views/Admin/Employee/List.vue
+++ src/views/Admin/Employee/List.vue
@@ -1,175 +1,91 @@
 <template>
-  <div class="p-10 gap-y-10">
-    <div class="text-3xl font-bold mb-10">
-      Employees
+    <div class="p-10 gap-y-10">
+        <div class="text-3xl font-bold mb-10">Employees</div>
+        <div class="flex mb-5 gap-4 items-center">
+            <v-btn color="primary">
+                Nouveau
+
+                <v-dialog
+                    v-model="isDialogOpen"
+                    activator="parent"
+                    width="auto"
+                >
+                    <v-card>
+                        <div class="p-5" id="dialogCreate">
+                            <h2 class="mb-4">Créer un utilisateur</h2>
+                            <v-text-field
+                                v-model="email"
+                                label="Email"
+                                outlined
+                            />
+                            <v-btn
+                                color="primary"
+                                block
+                                @click="handleSubmitNewUser"
+                                >Créer</v-btn
+                            >
+                        </div>
+                    </v-card>
+                </v-dialog>
+            </v-btn>
+            <v-text-field label="search" variant="outlined" hide-details />
+            <v-btn variant="flat">Filter</v-btn>
+        </div>
+        <div class="bg-red-100 flex-grow-1">
+            <v-data-table
+                :headers="headers"
+                :items="usersRolesRef"
+                class="elevation-1 border rounded-lg h-full"
+                height="600"
+                item-value="name"
+            ></v-data-table>
+        </div>
     </div>
-    <div class="flex mb-5 gap-4 items-center">
-      <router-link :to="{ name: 'admin-employees-create' }" class="p-2 hover:underline">
-        New employee
-      </router-link>
-      <v-text-field label="search" variant="outlined" hide-details/>
-      <v-btn variant="flat">Filter</v-btn>
-    </div>
-    <div class="bg-red-100 flex-grow-1">
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        class="elevation-1 border rounded-lg h-full"
-        height="600"
-        item-value="name"
-      ></v-data-table>
-    </div>
-  </div>
 </template>
 
-<script setup>
-import {ref} from "vue";
+<script lang="ts" setup>
+import { ref } from "vue";
+import { auth, usersRolesRef } from "@/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+const functions = getFunctions();
+
+const isDialogOpen = ref(false);
+const email = ref("");
+
+console.log(
+    "%cList.vue line:29 usersRolesRef",
+    "color: #007acc;",
+    usersRolesRef
+);
+
+const handleSubmitNewUser = () => {
+    // cors error policy
+    const addMessage = httpsCallable(functions, "helloWorld");
+    addMessage().then((result) => {
+        // Read result of the Cloud Function.
+        console.log("message", result);
+        // ...
+    });
+    isDialogOpen.value = false;
+};
 
 const headers = ref([
-  {
-    title: 'Dessert (100g serving)',
-    align: 'start',
-    sortable: false,
-    key: 'name',
-  },
-  { title: 'Calories', align: 'end', key: 'calories' },
-  { title: 'Fat (g)', align: 'end', key: 'fat' },
-  { title: 'Carbs (g)', align: 'end', key: 'carbs' },
-  { title: 'Protein (g)', align: 'end', key: 'protein' },
-  { title: 'Iron (%)', align: 'end', key: 'iron' },
-])
-const desserts = ref([
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    iron: '1',
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    iron: '1',
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    iron: '7',
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    iron: '8',
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    iron: '16',
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    iron: '0',
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    iron: '2',
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    iron: '45',
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    iron: '22',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: '6',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: '6',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: '6',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: '6',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: '6',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: '6',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: '6',
-  },
-])
+    {
+        title: "email",
+        align: "start",
+        // sortable: false,
+        key: "email",
+    },
+    { title: "Prénom", align: "end", key: "firstname" },
+    { title: "Nom", align: "end", key: "fat" },
+    { title: "Poste actuel", align: "end", key: "carbs" },
+]);
 </script>
 
 <style scoped>
-
+#dialogCreate {
+    width: 420px;
+}
 </style>
