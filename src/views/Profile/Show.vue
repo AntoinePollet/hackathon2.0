b8c0139eaa79ebd8f826fddfefe0ecf3855e5bf4
Antoine <pollet.antoine.alexis@gmail.com>
2023-06-09 15:44:53 +0200

    added back user profile avatar

--- src/views/Profile/Show.vue
+++ src/views/Profile/Show.vue
@@ -7,8 +7,9 @@
 
         <div v-if="user" class="grid grid-cols-4 pt-3">
             <div class="col-span-1">
-                <v-avatar size="120"
-                    image="https://images.unsplash.com/photo-1615807713086-bfc4975801d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=627&q=80"></v-avatar>
+                <div class="bg-cover h-32 w-32"
+                    :style="`background-image: url('https://api.multiavatar.com/${user.firstname}${user.lastname}.png?apikey=XdoCH30EA6grGx')`">
+                </div>
 
                 <div class="py-4">
                     <p class="font-semibold text-lg">{{ user?.firstname }} {{ user?.lastname }}</p>
