cde1d455167ab7e97f45fcbd203eb91172de0dae
Antoine <pollet.antoine.alexis@gmail.com>
2023-06-09 02:00:50 +0200

    Set NotFound into a component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Vue3Lottie
+            animationLink="https://assets7.lottiefiles.com/packages/lf20_u1xuufn3.json"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+<style scoped>
+div {
+    height: 100vh;
+}
+</style>
