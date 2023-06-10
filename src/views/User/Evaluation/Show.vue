d27b583549316fd9a4e089ff2d70dae9c75cd674
Michael De Madet <michaeldemadet@gmail.com>
2023-06-10 12:31:16 +0200

    add skill to user

--- src/views/User/Evaluation/Show.vue
+++ src/views/User/Evaluation/Show.vue
@@ -3,7 +3,7 @@
         <v-container v-if="user !== null">
             <h1 class="text-3xl font-bold mb-10">Évaluation</h1>
             <section class="mb-8" v-if="user !== null">
-                <div class="bg-white p-4 shadow flex items-center">
+                <div class="p-4 shadow flex items-center">
                     <div class="mr-4">
                         <v-avatar size="72">
                             <img :src="`https://api.multiavatar.com/${user.firstname}${user.lastname}.png?apikey=XdoCH30EA6grGx`"
@@ -15,7 +15,7 @@
                         <p class="text-lg">{{ user.role }}</p>
                     </div>
                     <div class="ml-auto"> <!-- Utilisation de ml-auto pour aligner le bouton à droite -->
-                        <v-btn prepend-icon="mdi-plus" color="primary" @click="addSkill" class="add-skill-btn">Ajouter une compétence</v-btn>
+                        <v-btn prepend-icon="mdi-plus" color="primary" @click="openAddSkillDialog" class="add-skill-btn">Ajouter une compétence</v-btn>
                     </div>
                 </div>
             </section>
@@ -42,13 +42,25 @@
             <v-btn color="primary" @click="updateSkillsRating" class="validate-btn mx-auto mt-10">Valider
                 l'évaluation</v-btn>
         </v-container>
+        <v-dialog v-model="addSkillDialog" max-width="500px">
+            <v-card>
+            <v-card-title>Ajouter une compétence</v-card-title>
+            <v-card-text>
+                <v-form @submit.prevent="addNewSkill">
+                    <v-text-field v-model="newSkill.name" label="Nom de la compétence"></v-text-field>
+                    <v-text-field v-model="newSkill.category" label="Catégorie de la compétence"></v-text-field>
+                    <v-btn type="submit" color="primary">Ajouter</v-btn>
+                </v-form>
+            </v-card-text>
+            </v-card>
+      </v-dialog>
     </div>
 </template>
   
 <script setup lang="ts">
 import { EventTypeEnum } from '@/interfaces/event';
 import { ref, onMounted } from 'vue';
-import { collection, getDocs, updateDoc, doc, query, where } from "firebase/firestore";
+import { collection, getDocs, updateDoc, doc, query, where, onSnapshot } from "firebase/firestore";
 import { firestoreDB } from "@/firebase";
 import { useRoute } from 'vue-router';
 import { useEventStore } from '@/stores/event'
@@ -59,17 +71,31 @@ const route = useRoute();
 const uuid = route.params.id;
 let user: any = ref(null);
 const skillMaxRating = 4;
+const addSkillDialog = ref(false);
+const newSkill = ref({ name: '', category: '' });
 
 onMounted(async () => {
     try {
-        const usersFromFB = await getDocs(collection(firestoreDB, "users"));
+        // Get users by uuid on snapshot
+        const q = query(collection(firestoreDB, 'users'), where('uuid', '==', uuid));
+        const querySnapshot = await getDocs(q);
 
-        usersFromFB.forEach((doc) => {
-            const data = doc.data();
-            if (data.uuid === uuid) {
-                user.value = data;
-            }
-        });
+        if (querySnapshot.size > 0) {
+            // Récupère l'ID du premier document correspondant à la requête
+            const docId = querySnapshot.docs[0].id;
+
+            const consultantRef = doc(firestoreDB, "users", docId);
+
+            onSnapshot(consultantRef, (doc) => {
+                if (doc.exists()) {
+                    user.value = {...doc.data(), id: doc.id};
+                } else {
+                    console.warn('Aucun document correspondant à l\'UUID spécifié n\'a été trouvé.');
+                }
+            });
+        } else {
+            console.warn('Aucun document correspondant à l\'UUID spécifié n\'a été trouvé.');
+        }
 
     } catch (error) {
         console.log(error);
@@ -109,6 +135,41 @@ const updateSkillsRating = async () => {
     } catch (error) {
         console.error('Erreur lors de la mise à jour des compétences :', error);
     }
+}
+
+const openAddSkillDialog = () => {
+    addSkillDialog.value = true;
+}
+
+const addNewSkill = async () => {
+  try {
+    const { name, category } = newSkill.value;
+
+    if (name && category) {
+        const skill = { name, category, rating: 1 };
+        
+        const userToEdit = doc(firestoreDB, "users", user.value.id);
+
+        await updateDoc(userToEdit, {
+            skills: [skill, ...user.value.skills]
+        });
+
+        // Fermer le dialogue et réinitialiser le formulaire
+        addSkillDialog.value = false;
+        newSkill.value = { name: '', category: '' };
+
+        createToast('La compétence a été ajoutée avec succès.', {
+            position: 'bottom-right',
+            type: 'success',
+            showIcon: true,
+            timeout: 2000,
+        });
+    } else {
+        console.warn('Veuillez remplir tous les champs du formulaire.');
+    }
+  } catch (error) {
+        console.error('Erreur lors de l\'ajout de la compétence :', error);
+  }
 }
 </script>
   
